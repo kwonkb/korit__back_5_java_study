ebf53731d3f0dccaa58a4a850b552c0abcb1e6d9
kwonkb <kwonkb98@gmail.com>
2024-01-02 16:26:26 +0900

    Revert "rm: 테스트용 파일삭제"
    This reverts commit 431c3f2536da9ea79c955e0e3a273257c0b125d8.

---
+++ src/main/java/com/study/git/git.java
@@ -0,0 +1,17 @@
+package com.study.git;
+
+public class git {
+    /*
+    * 파일의 버전 관리
+    * WD
+    * : 현재 작업중인 폴더
+    * remote : 외부 (github: 자신의 git을 클라우드로 저장시키는 역할)
+    * local : 내부 (git)
+    * git add 선택  git commit 저장
+    * 충돌, 버전 관리
+    * 형상 관리
+    * 프로토콜 , 단방향
+    * */
+
+
+}
