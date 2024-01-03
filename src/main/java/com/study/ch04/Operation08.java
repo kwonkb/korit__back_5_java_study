5fa2e9361f9e23f4a221c038ab008aa09ed9d149
kwonkb <kwonkb98@gmail.com>
2024-01-03 09:51:27 +0900

    복합대입연산자

---
+++ src/main/java/com/study/ch04/Operation08.java
@@ -0,0 +1,21 @@
+package com.study.ch04;
+
+public class Operation08 {
+    public static void main(String[] args) {
+
+
+        /*
+         * <<문제>>
+
+         * 복합대입연산 (산술연산, 대입연산)
+         * */
+
+        int num1 = 10;
+        int num2 = 20;
+        num1 = num1 + num2;
+        num1 += num2; //num1 + num2 해서 그 값을 num1에 저장
+        num1 *= num2;
+
+
+    }
+}
