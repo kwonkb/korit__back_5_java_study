8df7a14b7c1c51bfb91442063ff184ab26ff754d
kwonkb <kwonkb98@gmail.com>
2024-01-04 14:56:26 +0900

    for & class

--- src/main/java/com/study/ch06/for03.java
+++ src/main/java/com/study/ch06/for03.java
@@ -7,7 +7,7 @@ public class for03 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("별 몇개? ");
         int starCount = scanner.nextInt();
-
+//1.
         for (int i = 0; i < starCount; i++) {
             for (int j = 0; j < i + 1; j++) {
                 System.out.print("*");
@@ -37,5 +37,77 @@ public class for03 {
          *****
 
          */
+//2.
+        System.out.println();
+        for(int i = 0; i < starCount; i++){
+            for (int j = starCount; j > i; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        System.out.println();
+//3.
+        for (int i = 0; i < starCount; i++) {
+            for (int k = starCount - 1; k > i; k--){
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+//4.
+        for(int i = 0; i < starCount; i++){
+            for(int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for(int k = starCount; k > i; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        //5.
+        int num = 1;
+
+        for(int i = 0; i < starCount; i++){
+
+            for(int k = starCount - 1; k > i; k--){
+                System.out.print(" ");
+            }
+            for(int j = 0; j < num; j++){
+                System.out.print("*");
+            }
+            num = num + 2;
+            System.out.println();
+        }
+
+        System.out.println();
+        num = starCount * 2 - 1;
+
+        for(int i = 0; i < starCount; i++) {
+            for (int k = 0; k < i; k++){
+                System.out.print(" ");
+            }
+            for (int j = num; j > 0; j--){
+                System.out.print("*");
+            }
+            num = num - 2;
+            System.out.println();
+        }
+        System.out.println();
     }
 }
+
+
+
+
+
+
+
---
+++ src/main/java/com/study/ch07/ComputerMain.java
@@ -0,0 +1,29 @@
+package com.study.ch07;
+
+public class ComputerMain {
+    public static void main(String[] args) {
+        Computer computer1 = new Computer(); //인스턴스 생성 힙 메모리 할당
+        Computer computer2 = new Computer("노트북"); //인스턴스 생성 힙 메모리 할당
+
+        System.out.println(computer1);
+        System.out.println(computer2);
+
+
+        computer1.cpu = "i5";
+        computer2.cpu = "i7";
+
+        System.out.println(computer1.cpu);
+        System.out.println(computer2.cpu);
+        computer1.showInfo();
+        computer2.showInfo();
+
+        computer1.ram = "8GB";
+        computer2.ram = "16GB";
+
+        computer1.showInfo();
+        computer2.showInfo();
+
+        computer1.type = "데스크탑";
+
+    }
+}
