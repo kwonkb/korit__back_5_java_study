9bed70afe817ccb5e8ff8d40d97326025f0cfd9f
kwonkb <kwonkb98@gmail.com>
2024-01-05 14:18:35 +0900

    feat: 클래스 활용 & 배열

---
+++ src/main/java/com/study/ch08/member/ArrayMain02.java
@@ -0,0 +1,19 @@
+package com.study.ch08.member;
+
+public class ArrayMain02 {
+    public static void main(String[] args) {
+        String[] names = new String[4];
+        names[0] = "최수현";
+        names[1] = "전주환";
+        names[2] = "서창현";
+        names[3] = "심재원";
+
+
+
+        for(int i = 0; i <names.length; i++){
+        System.out.println(names[i]);
+        }
+
+    }
+
+}
